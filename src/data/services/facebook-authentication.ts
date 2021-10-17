d9ee481276753a95dd3b391aca1ebe72d559e540
Maycon Carlete <maycon.carlete@gmail.com>
2021-10-17 17:59:49 -0300

    feat: ensure Service calls CreateFacebookAccountRepository when LoadUserAccountRepository returns undefined

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,14 +1,24 @@
 import { FacebookAuthentication } from '@/domain/features'
-import { AuthenticationError } from '@/domain/errors'
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { AuthenticationError } from '@/domain/errors'
+import { CreateFacebookAccountRepository, LoadUserAccountRepository } from '@/data/contracts/repository'
 
 export class FacebookAuthenticationService implements FacebookAuthentication {
   constructor (
-    private readonly loadFacebookUserApi: LoadFacebookUserApi
+    private readonly loadFacebookUserApi: LoadFacebookUserApi,
+    private readonly loadUserAccountRepository: LoadUserAccountRepository,
+    private readonly createFacebookAccountRepository: CreateFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result> {
-    await this.loadFacebookUserApi.loadUser(params)
+    const facebookData = await this.loadFacebookUserApi.loadUser(params)
+    if (facebookData !== undefined) {
+      const repositoryData = await this.loadUserAccountRepository.load({ email: facebookData.email })
+      if (repositoryData === undefined) {
+        await this.createFacebookAccountRepository.createFromFacebook(facebookData)
+      }
+      return { result: { accessToken: facebookData.facebookId } }
+    }
     return { result: new AuthenticationError() }
   }
 }
--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,25 +1,57 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
-import { FacebookAuthenticationService } from '@/data/services'
+import { CreateFacebookAccountRepository, LoadUserAccountRepository } from '@/data/contracts/repository'
 import { AuthenticationError } from '@/domain/errors'
+import { FacebookAuthenticationService } from '@/data/services'
 
 class LoadFacebookUserSpy implements LoadFacebookUserApi {
   token?: string
   count=0
-  result = undefined
+  result: LoadFacebookUserApi.Result = undefined
   async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token
     this.count++
+    return new Promise((resolve, reject) => {
+      resolve(this.result)
+    })
+  }
+}
+
+class LoadUserAccountRepositorySpy implements LoadUserAccountRepository {
+  params={}
+  count=0
+  result = undefined
+  async load (params: LoadUserAccountRepository.Params): Promise<void> {
+    this.params = params
+    this.count++
     return this.result
+  }
+}
+
+class CreateFacebookAccountRepositorySpy implements CreateFacebookAccountRepository {
+  params = {}
+  count =0
+  async createFromFacebook (params: CreateFacebookAccountRepository.Params): Promise<void> {
+    this.params = params
+    this.count++
   }
 }
 
 describe('FacebookAuthenticationService', () => {
   let sut: FacebookAuthenticationService
   let loadFacebookUserApi: LoadFacebookUserSpy
+  let loadUserAccountRepository: LoadUserAccountRepositorySpy
+  let createFacebookAccountRepository: CreateFacebookAccountRepositorySpy
 
   beforeEach(() => {
     loadFacebookUserApi = new LoadFacebookUserSpy()
-    sut = new FacebookAuthenticationService(loadFacebookUserApi)
+    loadFacebookUserApi.result = {
+      name: ' any_facebook_name',
+      email: 'any_facebook_mail@mail.com',
+      facebookId: 'facebook_id'
+    }
+    loadUserAccountRepository = new LoadUserAccountRepositorySpy()
+    createFacebookAccountRepository = new CreateFacebookAccountRepositorySpy()
+    sut = new FacebookAuthenticationService(loadFacebookUserApi, loadUserAccountRepository, createFacebookAccountRepository)
   })
 
   it('should call LoadFacebookUserApi with correct params', async () => {
@@ -32,5 +64,21 @@ describe('FacebookAuthenticationService', () => {
     loadFacebookUserApi.result = undefined
     const authResult = await sut.perform({ token: 'invalid_token' })
     expect(authResult).toEqual({ result: new AuthenticationError() })
+  })
+
+  it('should call LoadUserAccountRepository when LoadFacebookUserApi returns data', async () => {
+    await sut.perform({ token: 'valid_token' })
+    expect(loadUserAccountRepository.params).toEqual({ email: 'any_facebook_mail@mail.com' })
+    expect(loadUserAccountRepository.count).toBe(1)
+  })
+
+  it('should call CreateFacebookAccountRepository when LoadUserAccountRepository returns undefined', async () => {
+    await sut.perform({ token: 'valid_token' })
+    expect(createFacebookAccountRepository.params).toEqual({
+      name: ' any_facebook_name',
+      email: 'any_facebook_mail@mail.com',
+      facebookId: 'facebook_id'
+    })
+    expect(createFacebookAccountRepository.count).toBe(1)
   })
 })
