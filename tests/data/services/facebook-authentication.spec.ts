6f79cb326b1ea406033bbdfcd1fabd13864a32ce
Maycon Carlete <maycon.carlete@gmail.com>
2021-10-17 10:39:57 -0300

    tests: change SUT creation strategy from factory to jest beforeEach

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -14,18 +14,22 @@ class LoadFacebookUserSpy implements LoadFacebookUserApi {
 }
 
 describe('FacebookAuthenticationService', () => {
+  let sut: FacebookAuthenticationService
+  let loadFacebookUserApi: LoadFacebookUserSpy
+
+  beforeEach(() => {
+    loadFacebookUserApi = new LoadFacebookUserSpy()
+    sut = new FacebookAuthenticationService(loadFacebookUserApi)
+  })
+
   it('should call LoadFacebookUserApi with correct params', async () => {
-    const loadFacebookUserApi = new LoadFacebookUserSpy()
-    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
     await sut.perform({ token: 'any_token' })
     expect(loadFacebookUserApi.token).toBe('any_token')
     expect(loadFacebookUserApi.count).toBe(1)
   })
 
   it('should return AuthenticationError when LoadFacebookUserApi return undefined', async () => {
-    const loadFacebookUserApi = new LoadFacebookUserSpy()
     loadFacebookUserApi.result = undefined
-    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
     const authResult = await sut.perform({ token: 'invalid_token' })
     expect(authResult).toEqual({ result: new AuthenticationError() })
   })
