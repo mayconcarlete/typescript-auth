ff640a8c1a0ce4e4dbc316e5939297056db6dfa3
Maycon Carlete <maycon.carlete@gmail.com>
2021-10-27 08:27:15 -0300

    test: ensure JwtTokenGenerator call jwt.sign with correct params

---
+++ tests/infra/crytpo/jwt-token-generator.spec.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+
+import { JwtTokenGenerator } from '@/infra/crypto'
+
+jest.mock('jsonwebtoken')
+
+describe('JwtTokenGenerator', () => {
+  it('should call sign with correct params', async () => {
+    const fakeJwt = jwt as jest.Mocked<typeof jwt>
+    const sut = new JwtTokenGenerator('any_secret')
+
+    await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+
+    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
+  })
+})
