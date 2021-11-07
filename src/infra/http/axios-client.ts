f9659ff39fae39262cee053d10ba94031448e4ad
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-07 15:42:17 -0300

    feat: return body of axios request

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -4,6 +4,7 @@ import { HttpGetClient } from '@/infra/http'
 
 export class AxiosHttpClient implements HttpGetClient {
   async get <T = any>(args: HttpGetClient.Params): Promise<T> {
-    return await axios.get(args.url, { params: args.params })
+    const result = await axios.get(args.url, { params: args.params })
+    return result.data
   }
 }
