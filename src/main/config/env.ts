44605c56ddbaa42141f454446853f5d9b678cbdf
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-07 15:43:29 -0300

    tests:ensure FacebookApi returns an User when token provided is valid

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '3123828511234482',
+    client_secret: process.env.FB_CLIENT_SECRET ?? '6e111b60c26f6af4b883e23ca451664a'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,4 +1,20 @@
-describe('', () => {
-  it('', () => {
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
+
+describe('FacebookApi integration test', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const httpClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      env.facebookApi.clientId,
+      env.facebookApi.client_secret,
+      httpClient
+    )
+    const facebookUser = await sut.loadUser({ token: 'EAAsZAGwDAWbIBADbVKI6UvLjAXUEkZBZCR4OxST6ZC4C6AtgNGTu8R4J6R1DrYLpcJJJc6zHh0tvM4cIJNiy6QZCmhkXMll5RbBLhdabVZBhpjwWavZA9qkGyZAZB2PmNtaoUTtCLwShaCn42yEpLsNCZAatxUO1K2VxskvS0OvN4CSWQIIKE3IoLlTELo646D7vYl3zECazD0gNkiwWu0w4pA' })
+    expect(facebookUser).toEqual({
+      name: 'Maycon Test',
+      email: 'maycon_bkvvjhu_test@tfbnw.net',
+      facebookId: '100135802492519'
+    })
   })
 })
