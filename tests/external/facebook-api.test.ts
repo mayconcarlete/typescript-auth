6b8c31f31755858a1db105bf681be873584a37e9
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-09 17:42:56 -0300

    test: add console.log to show facebookUser

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -16,8 +16,8 @@ describe('FacebookApi integration test', () => {
   })
 
   it('should return a Facebook User if token is valid', async () => {
-    const facebookUser = await sut.loadUser({ token: 'EAAsZAGwDAWbIBADbVKI6UvLjAXUEkZBZCR4OxST6ZC4C6AtgNGTu8R4J6R1DrYLpcJJJc6zHh0tvM4cIJNiy6QZCmhkXMll5RbBLhdabVZBhpjwWavZA9qkGyZAZB2PmNtaoUTtCLwShaCn42yEpLsNCZAatxUO1K2VxskvS0OvN4CSWQIIKE3IoLlTELo646D7vYl3zECazD0gNkiwWu0w4pA' })
-
+    const facebookUser = await sut.loadUser({ token: 'TOKEN_HERE' })
+    console.log(facebookUser)
     expect(facebookUser).toEqual({
       name: 'Maycon Test',
       email: 'maycon_bkvvjhu_test@tfbnw.net',
