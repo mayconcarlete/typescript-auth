313561aa0d82943558ad8a1906965125393fbbc1
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-12 08:18:14 -0300

    refactor: add type alias and object destructuring to simplify function signature

--- src/infra/crypto/jwt-token-generator.ts
+++ src/infra/crypto/jwt-token-generator.ts
@@ -1,13 +1,14 @@
 import { TokenGenerator } from '@/data/contracts/crypto'
 import { sign } from 'jsonwebtoken'
 
+type Params = TokenGenerator.Params
 export class JwtTokenGenerator implements TokenGenerator {
   constructor (
     private readonly secret: string
   ) {}
 
-  async generateToken (params: TokenGenerator.Params): Promise<string> {
-    const expirationInSeconds = params.expirationInMs / 1000
-    return sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
+  async generateToken ({ key, expirationInMs }: Params): Promise<string> {
+    const expirationInSeconds = expirationInMs / 1000
+    return sign({ key }, this.secret, { expiresIn: expirationInSeconds })
   }
 }
