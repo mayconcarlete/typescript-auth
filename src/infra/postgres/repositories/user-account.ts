e1c87edba799735f79e6afa1a4e3d35c3c477953
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-12 08:05:21 -0300

    refactor:  use destructuring assignment

--- src/infra/postgres/repositories/user-account.ts
+++ src/infra/postgres/repositories/user-account.ts
@@ -5,13 +5,13 @@ import { PgUser } from '@/infra/postgres/entities'
 
 type LoadParams = LoadUserAccountRepository.Params
 type LoadResult = LoadUserAccountRepository.Result
-
 type SaveParams = SaveFacebookAccountRepository.Params
 type SaveResult = SaveFacebookAccountRepository.Result
+
 export class PostgresUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
   private readonly pgUserRepository = getRepository(PgUser)
-  async load (params: LoadParams): Promise<LoadResult> {
-    const pgUser = await this.pgUserRepository.findOne({ email: params.email })
+  async load ({ email }: LoadParams): Promise<LoadResult> {
+    const pgUser = await this.pgUserRepository.findOne({ email })
     if (pgUser !== undefined) {
       return {
         id: pgUser.id.toString(),
@@ -20,24 +20,16 @@ export class PostgresUserAccountRepository implements LoadUserAccountRepository,
     }
   }
 
-  async saveWithFacebook (params: SaveParams): Promise<SaveResult> {
-    if (params.id === undefined) {
-      const account = await this.pgUserRepository.save({
-        email: params.email,
-        facebook_id: params.facebookId,
-        name: params.name
-      })
-      return {
-        id: account.id.toString()
-      }
+  async saveWithFacebook ({ email, facebookId, name, id }: SaveParams): Promise<SaveResult> {
+    let resultId: string
+    if (id === undefined) {
+      const account = await this.pgUserRepository.save({ email, facebookId, name })
+      resultId = account.id.toString()
+      return { id: resultId }
     } else {
-      await this.pgUserRepository.update({ id: parseInt(params.id) }, {
-        name: params.name,
-        facebook_id: params.facebookId
-      })
-      return {
-        id: params.id
-      }
+      resultId = id
+      await this.pgUserRepository.update({ id: parseInt(id) }, { name, facebook_id: facebookId })
+      return { id: resultId }
     }
   }
 }
