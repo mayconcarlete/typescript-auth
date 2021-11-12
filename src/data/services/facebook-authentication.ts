e3a451f6fa72bda9ac4d0282ee7db7253f393e6c
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-12 08:26:41 -0300

    refactor: add type alias and object destructuring to simplify function signature

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,9 +1,12 @@
-import { FacebookAuthentication } from '@/domain/features'
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
-import { AuthenticationError } from '@/domain/errors'
 import { SaveFacebookAccountRepository, LoadUserAccountRepository } from '@/data/contracts/repository'
+import { TokenGenerator } from '@/data/contracts/crypto'
+import { AuthenticationError } from '@/domain/errors'
+import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken, FacebookAccount } from '@/domain/models'
-import { TokenGenerator } from '../contracts/crypto'
+
+type Params = FacebookAuthentication.Params
+type Result = FacebookAuthentication.Result
 
 export class FacebookAuthenticationService implements FacebookAuthentication {
   constructor (
@@ -12,8 +15,8 @@ export class FacebookAuthenticationService implements FacebookAuthentication {
     private readonly crypto: TokenGenerator
   ) {}
 
-  async perform (params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result> {
-    const facebookData = await this.facebookApi.loadUser(params)
+  async perform ({ token }: Params): Promise<Result> {
+    const facebookData = await this.facebookApi.loadUser({ token })
     if (facebookData !== undefined) {
       const accountData = await this.userAccountRepository.load({ email: facebookData.email })
       const facebookAccount = new FacebookAccount(facebookData, accountData)
