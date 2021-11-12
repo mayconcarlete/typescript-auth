c67fba78821ae3e3fe8c818eed64b0206256cb21
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-12 09:02:06 -0300

    fix: move getRepository to inside functions

--- src/infra/postgres/repositories/user-account.ts
+++ src/infra/postgres/repositories/user-account.ts
@@ -9,9 +9,9 @@ type SaveParams = SaveFacebookAccountRepository.Params
 type SaveResult = SaveFacebookAccountRepository.Result
 
 export class PostgresUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
-  private readonly pgUserRepository = getRepository(PgUser)
   async load ({ email }: LoadParams): Promise<LoadResult> {
-    const pgUser = await this.pgUserRepository.findOne({ email })
+    const pgUserRepository = getRepository(PgUser)
+    const pgUser = await pgUserRepository.findOne({ email })
     if (pgUser !== undefined) {
       return {
         id: pgUser.id.toString(),
@@ -21,14 +21,15 @@ export class PostgresUserAccountRepository implements LoadUserAccountRepository,
   }
 
   async saveWithFacebook ({ email, facebookId, name, id }: SaveParams): Promise<SaveResult> {
+    const pgUserRepository = getRepository(PgUser)
     let resultId: string
     if (id === undefined) {
-      const account = await this.pgUserRepository.save({ email, facebookId, name })
+      const account = await pgUserRepository.save({ email, facebookId, name })
       resultId = account.id.toString()
       return { id: resultId }
     } else {
       resultId = id
-      await this.pgUserRepository.update({ id: parseInt(id) }, { name, facebook_id: facebookId })
+      await pgUserRepository.update({ id: parseInt(id) }, { name, facebook_id: facebookId })
       return { id: resultId }
     }
   }
