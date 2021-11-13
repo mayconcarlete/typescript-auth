c3bf4c752b8434baf4911a932e2f07624518f18d
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-13 00:06:26 -0300

    feat: create typeorm connection before starts api

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,7 +3,12 @@ import 'reflect-metadata'
 
 import env from './config/env'
 import { app } from './config/app'
+import { createConnection } from 'typeorm'
+import { config } from '../infra/postgres/helpers/config'
 
-app.listen(env.appPort, () => {
-  console.log(`Server is running at http://localhost:${env.appPort}`)
-})
+createConnection(config)
+  .then(() => {
+    app.listen(env.appPort, () => {
+      console.log(`Server is running at http://localhost:${env.appPort}`)
+    })
+  }).catch(console.error)
