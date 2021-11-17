d2e7e808f2f1ad4543d991d00964e1a26567af92
Maycon Carlete <maycon.carlete@gmail.com>
2021-11-17 16:08:51 -0300

    feat: isolate SUT creation

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -10,9 +10,13 @@ export class AuthenticationMiddleware {
 }
 
 describe('AuthenticationMiddleware', () => {
-  it('should return 403 if authorization is empty', async () => {
-    const sut = new AuthenticationMiddleware()
+  let sut: AuthenticationMiddleware
 
+  beforeEach(() => {
+    sut = new AuthenticationMiddleware()
+  })
+
+  it('should return 403 if authorization is empty', async () => {
     const httpResponse = await sut.handle({ authorization: '' })
 
     expect(httpResponse).toEqual({
@@ -21,17 +25,19 @@ describe('AuthenticationMiddleware', () => {
     })
   })
   it('should return 403 if authorization is null', async () => {
-    const sut = new AuthenticationMiddleware()
-
     const httpResponse = await sut.handle({ authorization: null as any })
 
-    expect(httpResponse).toEqual(forbidden())
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
   })
   it('should return 403 if authorization is undefined', async () => {
-    const sut = new AuthenticationMiddleware()
-
     const httpResponse = await sut.handle({ authorization: undefined as any })
 
-    expect(httpResponse).toEqual(forbidden())
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
   })
 })
